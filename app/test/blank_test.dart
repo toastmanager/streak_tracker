6ce9848783073065acfc67c890f81549eacef3b8
toastmanager <mrbonkyou@gmail.com>
2025-02-26 21:07:29 +0900

    fix: blank test without fail code

--- app/test/blank_test.dart
+++ app/test/blank_test.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Blank test to pass flutter test', () {
+    test('Should return Hello World!', () {
+      expect('Hello World!', 'Hello World!');
+    });
+  });
+}
